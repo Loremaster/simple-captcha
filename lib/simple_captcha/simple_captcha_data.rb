609eb44e7d56d6ffe7a09cd4dccbf4369e910e75
Serj L <gelvior@gmail.com>
2014-01-22 12:22:44 +0400

    Patch to make simple_captcha2 works on Rails 4 with protected_attributes.

--- lib/simple_captcha/simple_captcha_data.rb
+++ lib/simple_captcha/simple_captcha_data.rb
@@ -8,7 +8,7 @@ module SimpleCaptcha
     else
       set_table_name "simple_captcha_data"
     end
-    if ::ActiveRecord::VERSION::MAJOR == 3 and defined? attr_protected
+    if defined? attr_protected
       attr_protected
     end
 
